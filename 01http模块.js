0b1b79eac6129517748b85d170f70a853428570f
shijue1 <shengqiputixin@aliyun.com>
2019-05-08 13:27:44 +0800

    1

--- 01http模块.js
+++ 01http模块.js
@@ -6,6 +6,7 @@ const http = require('http')
 
 // 创建服务
 http.createServer((req, res) => {
+    
     res.writeHead(200, { 'Content-Type': 'text/html;charset="utf-8"'}) // 文件类型 html，字符集 utf-8
     // res.writeHead 需要写在 res.write() 之前，否则会报错：Provisional headers are shown
     
@@ -19,4 +20,5 @@ http.createServer((req, res) => {
     res.end() // 如果不加结束响应语句，标签页一直转圈，且第二次 favicon.ico 不会请求
     // res.write('。。。'); res.end() 可以写成 res.end('。。。')
     
-}).listen(1001, '127.0.0.1') //  '127.0.0.1' 可省略
+}).listen(1001) //  可填写 '127.0.0.1' 或 '192.168.100.137' ，也可省略，如果填写，前台必须由填写项发起请求，如：这里配置 127.0.0.1， 前台需要由 127.0.0.1 发起，
+                // 不可以由 192.168.100.137 发起，报错：Provisional headers are shown，如果省略 localhost '127.0.0.1' 或 '192.168.100.137' 都可发起
